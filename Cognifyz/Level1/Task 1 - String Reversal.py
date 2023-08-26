b53da21597d19c5b5fa2fc5cf0880fcbe9f371bb
Ratan Prajapati <125952103+abhi-ratan01@users.noreply.github.com>
2023-08-26 23:41:37 +0530

    Add files via upload

---
+++ Cognifyz/Level1/Task 1 - String Reversal.py
@@ -0,0 +1,7 @@
+# Function to return reverse string
+def stringreverse(string):
+    return string[::-1]
+
+string = input("Enter your string: ")
+# Function calling with the input string
+print(stringreverse(string))
